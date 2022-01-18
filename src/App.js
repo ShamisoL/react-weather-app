6ae99118be6d5e40790a8a7cf6c44eeda47e0827
ShamisoL <86979110+ShamisoL@users.noreply.github.com>
2022-01-18 18:34:36 +0000

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project iwas coded by Shamiso Lewis and is{" "}
-        <a
-          href="https://github.com/ShamisoL/react-weather-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+        <footer>
+          This project was coded by Shamiso Lewis and is{" "}
+          <a
+            href="https://github.com/ShamisoL/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
